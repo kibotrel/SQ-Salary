d0fa7f19b09970478e6781a00d6c1e38c8fd934e
BOTREL Kilian <botrel.kilian1999@gmail.com>
2020-03-23 04:49:20 +0100

    instanceCreate command implemented

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 
 var		major			= "0";
-var		minor			= ".0.1";
+var		minor			= ".0.2";
 var		prefix			= "!salary";
+
+var		instanceID		= 0;
+var		player			= [];
+var		playerList		= [];
 const	Discord			= require('discord.js');
 const	client  		= new Discord.Client();
 
@@ -11,8 +15,6 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	var request			= message.content.substring(prefix.length).trim().split(" ");
-	var	description		= [];
 	var	embedMessage	= new Discord.MessageEmbed();
 
 	embedMessage.setColor("#010101")
@@ -22,6 +24,8 @@ client.on('message', message => {
 				.setTimestamp();
 	if (message.content.startsWith(prefix))
 	{
+		var request	= message.content.substring(prefix.length).trim().split(" ");
+
 		for (i = 0; i < request.length; i++)
 		{
 			if (request[i] == 0)
@@ -32,6 +36,42 @@ client.on('message', message => {
 		}
 		if (!request.length)
 			message.channel.send("**Undefined command!**. Use: `" + prefix + " help`");
+		else if (request[0] === "help")
+		{
+			embedMessage.setDescription(`Here are the possible **commands** :\n\n\
+			- Register a new instance in the database\n\`${prefix} newInstance [player1-player2-...-playerN]\`\n\n\
+			- Edit the drop worth of a player\n\`${prefix} addWorth playerName value\``);
+			message.channel.send(embedMessage);
+		}
+		else if (request[0] === "newInstance")
+		{
+			if (typeof request[1] !== "undefined")
+			{
+				var	user	= request[1].split("-");
+				for (i = 0; i < user.length; i++)
+				{
+					if (user[i] == 0)
+					{
+						user.splice(i, 1);
+						i = 0;
+					}
+				}
+				var username;
+				for (username of user)
+				{
+					var userIndex	= playerList.indexOf(username);
+					if (userIndex === -1)
+					{
+						playerList.push(username);
+						player.push({name:username, instanceCount:1, balanceWorth:0});
+					}
+					else
+						player[userIndex].instanceCount++;
+				}
+				message.channel.send("Instance **#" + instanceID + "** created!");
+				instanceID++;
+			}
+		}
 	}
 });
 client.login(process.env.DISCORD_TOKEN);
