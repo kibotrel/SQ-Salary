7798ea78e6256e18645d8820bec73042f736f7bb
BOTREL Kilian <botrel.kilian1999@gmail.com>
2020-04-04 15:51:50 +0200

    Removed useless else case

--- commands/changePrefix.js
+++ commands/changePrefix.js
@@ -5,15 +5,15 @@ exports.run = (client, message, args) => {
 	//Retrieve the right prefix sequence.
 	const	oldPrefix = Database.getPrefix(message.guild.id);
 
-	//Check if a new prefix is sent by the user and update it.
+	//Check if a new prefix is sent.
 	if (typeof args[0] === "undefined")
 	{
 		message.channel.send(`**Undefined argument(s)!** Use: \`${oldPrefix} changePrefix newPrefix\`.`);
 		return ;
 	}
-	else
-	{
-		const	newPrefix = Database.changePrefix(message.guild.id, args[0]);
-		message.channel.send(`**Command prefix Successfuly updated!** Use \`${newPrefix}\` to interract with the bot now.`);
-	}
+
+	//Update prefix.
+	const	newPrefix = Database.changePrefix(message.guild.id, args[0]);
+
+	message.channel.send(`**Command prefix Successfuly updated!** Use \`${newPrefix}\` to interract with the bot now.`);
 }
