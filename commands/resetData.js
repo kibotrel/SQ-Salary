27faf3259bbcea090358b6493532d00036d00e7e
BOTREL Kilian <botrel.kilian1999@gmail.com>
2020-04-09 02:49:30 +0200

    Closes #2

--- commands/resetData.js
+++ commands/resetData.js
@@ -1,7 +1,7 @@
 //Load database informations.
 const	Database = require("../database.js");
 
-exports.run = (client, message, args) => {
+exports.run = (client, message) => {
 	Database.resetServer(message.guild.id);
 	message.channel.send(`Registered data successfully cleared!`);
 }
